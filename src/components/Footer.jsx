7b0fb7c1c1c55946bb0fc67910504a66b237a156
Mishael <Mishaeldada@gmail.com>
2023-09-03 06:20:57 +0100

    changed footer color

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <div className="mt-8 bg-[#067CA6]">
+    <div className="mt-8 bg-[#009FE3]">
       <div className="app-container py-8 text-center text-white">
         <p className="font-bold text-[1.3rem]">Copyright Phillips Akindele and Co 2023</p>
       </div>
