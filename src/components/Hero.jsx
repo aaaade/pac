3442e1b1178fa1ddc8b234200adbb5e010769166
Mishael <Mishaeldada@gmail.com>
2023-09-05 11:19:10 +0100

    fixed bg

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -5,7 +5,7 @@ function Hero() {
     <div className="hero relative" id="home">
       <div className="app-container">
         <div className="flex items-center h-[80vh]">
-          <h1 className="text-5xl max-w-[100%] md:max-w-[700px] font-semibold leading-30 bg-[#009FE3] p-8 text-white">Your <span className="font-bold">Dependable Independent Professionals</span> </h1>
+          <h1 className="text-3xl max-w-[100%] md:max-w-[700px] font-semibold leading-30 bg-[#009FE3] p-8 py-2 text-white">Your <span className="font-bold">Dependable Independent Professionals</span> </h1>
         </div>
       </div>
     </div>
